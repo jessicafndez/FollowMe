016a30fbed12896e3da8aaef9878230c9717dbe1
Mona Lisa <jessica@hdesking.com>
2018-01-11 17:37:52 +0100

    First Commit

---
+++ app/src/main/java/com/idlebeat/fome/FollowActivity.java
@@ -0,0 +1,19 @@
+package com.idlebeat.fome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by devel on 11/01/2018.
+ */
+
+public class FollowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.follow);
+
+    }
+
+}
